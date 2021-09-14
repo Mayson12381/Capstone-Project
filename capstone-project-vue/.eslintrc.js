53b4dced111c9468b4a62b36bc1949c9726a2fcf
patrick <patrick-weber@outlook.com>
2021-09-14 12:52:07 +0200

    :fire: :wrench: [web] - test: initialize test library

--- capstone-project-vue/.eslintrc.js
+++ capstone-project-vue/.eslintrc.js
@@ -30,8 +30,9 @@ module.exports = {
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
+        "**/__tests__/*.{j,t}s?n(x)",
         "**/tests/unit/**/*.spec.{j,t}s?(x)",
+        "**spec.ts",
       ],
       env: {
         jest: true,
